d65f90be14def2f92c728199b4cd3e3cfaf0924b
alizahir23 <alizahiruddin@gmail.com>
2020-05-07 01:08:19 +0530

    fixed redirecting issue on home page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../css/style.css";
+import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <header>
       <div className="logo">
-        <a href="../public/index.html">OPEN<span>&lt;SOURCE&gt;</span>CODE</a>
+        <Link to="/">OPEN<span>&lt;SOURCE&gt;</span>CODE</Link>
         <a href="javascript:void(0);" className="icon" onclick="myFunction()">
           <i className="fa fa-bars"></i>
         </a>
