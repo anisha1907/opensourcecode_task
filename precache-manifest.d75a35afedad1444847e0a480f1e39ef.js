36c828d214407dadb50c52ff4a0adc46c023a38e
Arya Soni <18515597+aryasoni98@users.noreply.github.com>
2020-05-06 15:29:48 +0530

    Updates

---
+++ precache-manifest.d75a35afedad1444847e0a480f1e39ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3562d331e0c31fe068fc2b3b16785883",
+    "url": "/osc_app/index.html"
+  },
+  {
+    "revision": "899d93afaf7236f97231",
+    "url": "/osc_app/static/css/main.982a28f2.chunk.css"
+  },
+  {
+    "revision": "c6b67fba9f6be71e5ba8",
+    "url": "/osc_app/static/js/2.8fae91a5.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/osc_app/static/js/2.8fae91a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899d93afaf7236f97231",
+    "url": "/osc_app/static/js/main.142392f4.chunk.js"
+  },
+  {
+    "revision": "026958e72850b3eb67aa",
+    "url": "/osc_app/static/js/runtime-main.284679f3.js"
+  },
+  {
+    "revision": "93a02324efae2e4176b0b85acc81e9e0",
+    "url": "/osc_app/static/media/about-left-SVG.93a02324.png"
+  },
+  {
+    "revision": "6b0d48495889fea3582e584112fb49d2",
+    "url": "/osc_app/static/media/how-right-SVG.6b0d4849.png"
+  },
+  {
+    "revision": "c695fc67ae811011c1202a22ed4e8a72",
+    "url": "/osc_app/static/media/left-SVG.c695fc67.png"
+  },
+  {
+    "revision": "9e6d3626ed23e4f6502b8ce7afdf99fd",
+    "url": "/osc_app/static/media/right-svg.9e6d3626.png"
+  }
+]);
